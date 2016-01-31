cf8ac1bd2afc796fe4b347c46ae462bc47bca559
Samantha Morocz <samanthamorocz@gmail.com>
2016-01-31 18:30:22 -0500

    Everything works but maps tags or windows

--- client/app/controllers/listings.client.controller.js
+++ client/app/controllers/listings.client.controller.js
@@ -15,17 +15,14 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
     };
 
     $scope.findOne = function() {
-      debugger;
+     // debugger;
       $scope.loading = true;
 
       /*
         Take a look at 'list-listings.client.view', and find the ui-sref attribute that switches the state to the view 
         for a single listing. Take note of how the state is switched: 
-
           ui-sref="listings.view({ listingId: listing._id })"
-
         Passing in a parameter to the state allows us to access specific properties in the controller.
-
         Now take a look at 'view-listing.client.view'. The view is initialized by calling "findOne()". 
         $stateParams holds all the parameters passed to the state, so we are able to access the id for the 
         specific listing we want to find in order to display it to the user. 
@@ -74,18 +71,13 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
     };
 
     $scope.update = function(isValid) {
-      /*
-        Fill in this function that should update a listing if the form is valid. Once the update has 
-        successfully finished, navigate back to the 'listing.list' state using $state.go(). If an error 
-        occurs, pass it to $scope.error. 
-       */
+      Listings.update($scope.listing._id, $scope.listing);
     };
 
     $scope.remove = function() {
-      /*
-        Implement the remove function. If the removal is successful, navigate back to 'listing.list'. Otherwise, 
-        display the error. 
-       */
+
+      Listings.delete($scope.listing._id);
+
     };
 
     /* Bind the success message to the scope if it exists as part of the current state */
@@ -101,5 +93,85 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
       }, 
       zoom: 14
     }
+
+    $scope.$watch(function() {
+      return $scope.map.bounds;
+    }, function(nv, ov) {
+      // Only need to regenerate once
+      if (!ov.southwest && nv.southwest) {
+        var markers = [];
+        for (var i = 0; i < 50; i++) {
+          markers.push($scope.listingMarkers[i]);
+        }
+        $scope.randomMarkers = markers;
+      }
+    }, true);
+
+    // $scope.getCoordinates = function() {
+    //   Listings.getAll().then(function(response) {
+    //     $scope.listings = response.data;
+    //     var markers = [];
+    //     console.log('Listings: ' + JSON.stringify($scope.listings) );
+    //     for (var i in $scope.listings) {
+    //       var tempListing = $scope.listings[i];
+    //       console.log('Lat: ' + tempListing.coordinates.latitude + '\tLong: ' + tempListing.coordinates.longitude + '\n');
+    //       if (tempListing.coordinates !== undefined && tempListing.coordinates !== null) {
+    //         console.log('Lat: ' + tempListing.coordinates.latitude + '\tLong: ' + tempListing.coordinates.longitude + '\n');
+    //         var coords = {
+    //           latitude: tempListing.coordinates.latitude,
+    //           longitude: tempListing.coordinates.longitude,
+    //           title: tempListing.name
+    //         };
+    //         coords[idKey] = i;
+    //         markers.push(coords);
+    //       }
+    //     }
+    //     $scope.listingMarkers = markers;
+    //   }, function(error) {
+    //     $scope.loading = false;
+    //     $scope.error = 'Unable to retrieve listings!\n' + error;
+    //   });
+
+    //   };
+
+      var createRandomMarker = function(i, bounds, idKey) {
+        var lat_min = bounds.southwest.latitude,
+          lat_range = bounds.northeast.latitude - lat_min,
+          lng_min = bounds.southwest.longitude,
+          lng_range = bounds.northeast.longitude - lng_min;
+
+        if (idKey == null) {
+          idKey = "id";
+        }
+
+        var myListing = $scope.listings[i];
+        var latitude = lat_min + (Math.random() * lat_range);
+        var longitude = lng_min + (Math.random() * lng_range);
+        var ret = {
+          latitude: latitude,
+          longitude: longitude,
+          title: 'm' + i
+        };
+        ret[idKey] = i;
+        return ret;
+      };
+      $scope.randomMarkers = [];
+      // Get the bounds from the map once it's loaded
+      $scope.$watch(function() {
+        return $scope.map.bounds;
+      }, function(nv, ov) {
+        // Only need to regenerate once
+        if (!ov.southwest && nv.southwest) {
+          var markers = [];
+          for (var i = 0; i < 50; i++) {
+            markers.push(createRandomMarker(i, $scope.map.bounds))
+          }
+          $scope.randomMarkers = markers;
+        }
+      }, true);
+    });
   }
+
+
+
 ]);
